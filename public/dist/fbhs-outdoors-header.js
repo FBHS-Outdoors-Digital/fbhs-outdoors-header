d7a18d63702784cbc1f2e7e4ffe59447edbdf79f
isaac long <54558961+isaaclong26@users.noreply.github.com>
2022-10-17 09:38:59 -0400

    Update fbhs-outdoors-header.js

--- public/dist/fbhs-outdoors-header.js
+++ public/dist/fbhs-outdoors-header.js
@@ -37,7 +37,7 @@
         containerWidth: '1190px',
         containerBackground: '#eee',
         containerPadding: '0',
-        target: 'bodyr'
+        target: 'body'
       },
       'qa.thermatru.com': {
         activeBackgroundColor: '#4b4c4e',
